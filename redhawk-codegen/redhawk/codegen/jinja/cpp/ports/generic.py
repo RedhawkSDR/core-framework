521be25cf899847e0737dbf68818dae3d1511279
8530 <8530@vm-131.vsi-corp.com>
2017-04-14 08:27:47 -0400

    add debug header for custom output ports, CF-1743

--- redhawk-codegen/redhawk/codegen/jinja/cpp/ports/generic.py
+++ redhawk-codegen/redhawk/codegen/jinja/cpp/ports/generic.py
@@ -226,7 +226,7 @@ class GenericPortGenerator(CppPortGenerator):
         return (cpp.stringLiteral(name), 'this')
 
     def dependencies(self):
-        return ['<boost/thread/locks.hpp>', '<ossie/Port_impl.h>', cpp.idlHeader(self.idl)]
+        return ['<boost/thread/locks.hpp>', '<ossie/Port_impl.h>', '<ossie/debug.h>', cpp.idlHeader(self.idl)]
 
     def loader(self):
         return jinja2.PackageLoader(__package__)
