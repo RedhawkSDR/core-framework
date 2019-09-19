1a68311d50ad4c6415f9f2335b247688867842d7
Padric Hall <padric.hall@omnibusamerica.com>
2019-09-19 12:59:32 -0400

    CCB-2141

---
+++ redhawk/src/testing/tests/test_SAD_xml.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+#
+# This file is protected by Copyright. Please refer to the COPYRIGHT file
+# distributed with this source distribution.
+#
+# This file is part of REDHAWK bulkioInterfaces.
+#
+# REDHAWK bulkioInterfaces is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# REDHAWK bulkioInterfaces is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+
+from base_ports  import *
+from  bulkio import *
+from ossie.cf import CF
+from omniORB import any
+from ossie.utils import sb
+import os, time
+
+
+class Test_SADxml(unittest.TestCase):
+    def __init__( self, methodName='runTest' ):
+        unittest.TestCase.__init__(self, methodName)
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_iohelper(self):
+        t1 = sb.launch('../components/Python_Ports/Python_Ports.spd.xml')
+        t2 = sb.launch('../components/Python_Ports/Python_Ports.spd.xml')
+        source = sb.StreamSource()
+        destination = sb.StreamSink()
+        source.connect(t1, usesPortName="xmlOut")
+        t2.connect(destination, providesPortName="xmlIn")
+        sb.generateSADXML("testsadxml")
+
+
+if __name__ == '__main__':
+    suite = unittest.TestSuite()
+    for x in [ Test_SADxml ]:
+        tests = unittest.TestLoader().loadTestsFromTestCase(x)
+        suite.addTests(tests)
+    try:
+        import xmlrunner
+        runner = xmlrunner.XMLTestRunner(verbosity=2)
+    except ImportError:
+        runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
