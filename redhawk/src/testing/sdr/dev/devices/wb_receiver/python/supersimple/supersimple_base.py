d51e7e6bd228b9c9ad7791233d390128a8fff997
Max Robert <max.robert@geontech.com>
2019-11-12 15:46:57 -0500

    add device that dynamically deploys devices

---
+++ redhawk/src/testing/sdr/dev/devices/wb_receiver/python/supersimple/supersimple_base.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+#
+# AUTO-GENERATED CODE.  DO NOT MODIFY!
+#
+# Source: None
+from ossie.cf import CF
+from ossie.cf import CF__POA
+from ossie.utils import uuid, model
+
+from ossie.device import Device
+from ossie.threadedcomponent import *
+from ossie.properties import simple_property
+
+import Queue, copy, time, threading
+from ossie.resource import usesport, providesport, PortCallError
+import bulkio
+import frontend
+from frontend import FRONTEND
+from omniORB import any as _any
+from ossie.componentfamily import DynamicComponent
+
+class supersimple_base(CF__POA.Device, Device, ThreadedComponent, DynamicComponent):
+        # These values can be altered in the __init__ of your derived class
+
+        PAUSE = 0.0125 # The amount of time to sleep if process return NOOP
+        TIMEOUT = 5.0 # The amount of time to wait for the process thread to die when stop() is called
+        DEFAULT_QUEUE_SIZE = 100 # The number of BulkIO packets that can be in the queue before pushPacket will block
+
+        def __init__(self, devmgr, uuid, label, softwareProfile, compositeDevice, execparams):
+            Device.__init__(self, devmgr, uuid, label, softwareProfile, compositeDevice, execparams)
+            ThreadedComponent.__init__(self)
+            DynamicComponent.__init__(self)
+
+            # self.auto_start is deprecated and is only kept for API compatibility
+            # with 1.7.X and 1.8.0 devices.  This variable may be removed
+            # in future releases
+            self.auto_start = False
+            # Instantiate the default implementations for all ports on this device
+            self.port_foo = bulkio.InFloatPort("foo", maxsize=self.DEFAULT_QUEUE_SIZE)
+            self.port_foo._portLog = self._baseLog.getChildLogger('foo', 'ports')
+            self.port_bar = bulkio.OutFloatPort("bar")
+            self.port_bar._portLog = self._baseLog.getChildLogger('bar', 'ports')
+
+        def setParentInstance(self, _pI):
+            self._parentInstance = _pI
+
+        def start(self):
+            Device.start(self)
+            ThreadedComponent.startThread(self, pause=self.PAUSE)
+
+        def stop(self):
+            Device.stop(self)
+            if not ThreadedComponent.stopThread(self, self.TIMEOUT):
+                raise CF.Resource.StopError(CF.CF_NOTSET, "Processing thread did not die")
+
+        def releaseObject(self):
+            try:
+                self.stop()
+            except Exception:
+                self._baseLog.exception("Error stopping")
+            Device.releaseObject(self)
+
+        ######################################################################
+        # PORTS
+        # 
+        # DO NOT ADD NEW PORTS HERE.  You can add ports in your derived class, in the SCD xml file, 
+        # or via the IDE.
+
+        port_foo = providesport(name="foo",
+                                repid="IDL:BULKIO/dataFloat:1.0",
+                                type_="control")
+
+        port_bar = usesport(name="bar",
+                            repid="IDL:BULKIO/dataFloat:1.0",
+                            type_="control")
+
+        ######################################################################
+        # PROPERTIES
+        # 
+        # DO NOT ADD NEW PROPERTIES HERE.  You can add properties in your derived class, in the PRF xml file
+        # or by using the IDE.
+        device_kind = simple_property(id_="DCE:cdc5ee18-7ceb-4ae6-bf4c-31f983179b4d",
+                                      name="device_kind",
+                                      type_="string",
+                                      mode="readonly",
+                                      action="eq",
+                                      kinds=("allocation",),
+                                      description="""This specifies the device kind""")
+
+
+        device_model = simple_property(id_="DCE:0f99b2e4-9903-4631-9846-ff349d18ecfb",
+                                       name="device_model",
+                                       type_="string",
+                                       mode="readonly",
+                                       action="eq",
+                                       kinds=("allocation",),
+                                       description=""" This specifies the specific device""")
+
+
+        abc = simple_property(id_="abc",
+                              name="abc",
+                              type_="string",
+                              mode="readwrite",
+                              action="external",
+                              kinds=("property",))
+
+
+
+
+        def removeAllocationIdRouting(self,tuner_id):
+            pass
+
---
+++ redhawk/src/testing/sdr/dev/devices/wb_receiver/tests/test_wb_receiver.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import ossie.utils.testing
+from ossie.utils import sb
+
+class DeviceTests(ossie.utils.testing.RHTestCase):
+    # Path to the SPD file, relative to this file. This must be set in order to
+    # launch the device.
+    SPD_FILE = '../wb_receiver.spd.xml'
+
+    # setUp is run before every function preceded by "test" is executed
+    # tearDown is run after every function preceded by "test" is executed
+    
+    # self.comp is a device using the sandbox API
+    # to create a data source, the package sb contains sources like StreamSource or FileSource
+    # to create a data sink, there are sinks like StreamSink and FileSink
+    # to connect the component to get data from a file, process it, and write the output to a file, use the following syntax:
+    #  src = sb.FileSource('myfile.dat')
+    #  snk = sb.StreamSink()
+    #  src.connect(self.comp)
+    #  self.comp.connect(snk)
+    #  sb.start()
+    #
+    # components/sources/sinks need to be started. Individual components or elements can be started
+    #  src.start()
+    #  self.comp.start()
+    #
+    # every component/elements in the sandbox can be started
+    #  sb.start()
+
+    def setUp(self):
+        # Launch the device, using the selected implementation
+        self.comp = sb.launch(self.spd_file, impl=self.impl)
+    
+    def tearDown(self):
+        # Clean up all sandbox artifacts created during test
+        sb.release()
+
+    def testBasicBehavior(self):
+        #######################################################################
+        # Make sure start and stop can be called without throwing exceptions
+        self.comp.start()
+        self.comp.stop()
+        #raw_input('press any key')
+
+if __name__ == "__main__":
+    ossie.utils.testing.main() # By default tests all implementations
