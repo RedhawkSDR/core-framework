7571d47725ec4a0a04b5c33bae5cd1fe47a6ef3d
Max Robert <max.robert@geontech.com>
2020-04-23 14:16:34 -0400

    Refs CF-2165. Add busy check to base classes

--- redhawk/src/testing/sdr/dev/devices/devj/java/src/devj/java/devj.java
+++ redhawk/src/testing/sdr/dev/devices/devj/java/src/devj/java/devj.java
@@ -2,6 +2,8 @@ package devj.java;
 import helloworld.HelloWorld;
 
 import java.util.Properties;
+import org.ossie.properties.PropertyListener;
+import CF.DevicePackage.UsageType;
 
 /**
  * This is the device code. This file contains the derived class where custom
@@ -79,7 +81,23 @@ public class devj extends devj_base {
     public devj()
     {
         super();
+        System.out.println("------------------ i'm up");
         HelloWorld hw = new HelloWorld();
+        this.busy_state.addChangeListener(new PropertyListener<Boolean>() {
+            public void valueChanged(Boolean oldValue, Boolean newValue) {
+                busyStateValueChanged(oldValue, newValue);
+            }
+        });
+    }
+
+    private void busyStateValueChanged(Boolean oldValue, Boolean newValue)
+    {
+        this.busy_state.setValue(newValue);
+        if (newValue) {
+            this.setUsageState(UsageType.BUSY);
+        } else {
+            this.setUsageState(UsageType.IDLE);
+        }
     }
 
     /**************************************************************************
