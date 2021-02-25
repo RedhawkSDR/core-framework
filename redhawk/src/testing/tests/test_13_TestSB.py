25fd6d6e09100e32ec4fde23bd822ebe51b02fcf
kvjohns <kvjohns@vm.vsi-corp.com>
2021-02-25 08:15:14 -0500

    CCB-1172-process-leaked adding component and modified test

--- redhawk/src/testing/tests/test_13_TestSB.py
+++ redhawk/src/testing/tests/test_13_TestSB.py
@@ -34,7 +34,10 @@ import struct
 import tempfile
 import numpy
 import psutil
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 from omniORB import CORBA, any, tcInternal
 
 from ossie import properties
@@ -303,6 +306,7 @@ class SBStdOutTest(scatest.CorbaTestCase):
 class SBInitCompTest(scatest.CorbaTestCase):
     def test_cleanup(self):
         comp = sb.launch('sdr/dom/components/TestCompInit/test_componentinit.spd.xml') 
+<<<<<<< Updated upstream
         pid = comp._pid
         ppid = 0 
         if sb.domainless._sandbox:
@@ -311,6 +315,13 @@ class SBInitCompTest(scatest.CorbaTestCase):
         result = psutil.pid_exists(pid)
         self.assertEquals(result, False)
 
+=======
+        process_id = comp._pid 
+        if sb.domainless._sandbox:
+            sb.domainless._sandbox.shutdown()
+            sb.domainless._sandbox = None
+        self.assertFalse(psutil.pid_exists(process_id))
+>>>>>>> Stashed changes
 class SBTestTest(scatest.CorbaTestCase):
     def setUp(self):
         sb.setDEBUG(False)
