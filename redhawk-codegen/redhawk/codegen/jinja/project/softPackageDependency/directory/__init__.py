c9643a82e7db9a1e430124060d0bc66ada67d315
8530 <8530@vm.vsi-corp.com>
2021-07-16 13:49:11 -0400

    missing 2to3 gend files

--- redhawk-codegen/redhawk/codegen/jinja/project/softPackageDependency/directory/__init__.py
+++ redhawk-codegen/redhawk/codegen/jinja/project/softPackageDependency/directory/__init__.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 
-from generator import DependencyDirectoryProjectGenerator
+from .generator import DependencyDirectoryProjectGenerator
 
 def factory(**opts):
     return DependencyDirectoryProjectGenerator(**opts)
