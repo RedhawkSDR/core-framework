173f006af9c6fcb89e1a58fdd054731d61d6295e
Jon McGee <jmcgee@koffing.office.geontechnologies.com>
2021-07-29 12:41:53 -0400

    Fixed the cfg parser path

--- redhawk/src/base/framework/python/ossie/utils/sandbox/clusterCfgParser.py
+++ redhawk/src/base/framework/python/ossie/utils/sandbox/clusterCfgParser.py
@@ -3,12 +3,12 @@ import os
 import configparser
 
 OSSIEHOME = os.getenv("OSSIEHOME")
-CLUSTER_FILE = OSSIEHOME + "/aws/cluster.cfg"
+CLUSTER_FILE = OSSIEHOME + "/cluster.cfg"
 
 class ClusterCfgParser():
     def __init__(self, clusterName=None):
         config = configparser.ConfigParser()
-        cluster_file = OSSIEHOME + '/aws/cluster.cfg'
+        cluster_file = OSSIEHOME + '/cluster.cfg'
         data = open(cluster_file, 'r').read()
         config.read(cluster_file)
         if clusterName is None:
