4357bd5e895d98d28ee6e420aaeb944bd8116f05
Max Robert <mrobert@geontech.com>
2023-01-20 14:12:42 +0000

    Refs #171. initialize external properties

--- redhawk/src/base/framework/python/ossie/utils/sb/domainless.py
+++ redhawk/src/base/framework/python/ossie/utils/sb/domainless.py
@@ -835,6 +835,14 @@ def loadSADFile(filename, props={}):
                         props.pop(simple.refid)
                     container = overloadContainer(str(simple.refid),overload_value)
                     simple_exec_vals[container.id] = container.value
+                for ep in externprops:
+                    if ep['comprefid'] != instanceID:
+                        continue
+                    if ep['propid'] in execprops and ep['externalpropid'] in props:
+                        overload_value = props[ep['externalpropid']]
+                        props.pop(ep['externalpropid'])
+                        container = overloadContainer(str(ep['propid']), overload_value)
+                        simple_exec_vals[container.id] = container.value
                 # If AC execparam property is overriden in props but not SAD file, update value
                 for prop in list(props):
                     if prop in execprops and instanceID == assemblyControllerRefid:
