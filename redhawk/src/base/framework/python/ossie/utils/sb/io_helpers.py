4a732cdfc292757c48a84a5300053d7d1a46f36d
Max Robert <mrobert@geontech.com>
2023-02-02 21:06:23 +0000

    Refs #178. changing the port definition for B in midas files breaks compatibility and is against policy

--- redhawk/src/base/framework/python/ossie/utils/sb/io_helpers.py
+++ redhawk/src/base/framework/python/ossie/utils/sb/io_helpers.py
@@ -887,7 +887,7 @@ class FileSource(_SourceBase):
         if self._midasFile:
             hdr, data = _bluefile.read(self._filename, list)
             if hdr['format'].endswith('B'):
-                dataFormat = 'octet'
+                dataFormat = 'char'
             elif hdr['format'].endswith('I'):
                 dataFormat = 'short'
             elif hdr['format'].endswith('L'):
